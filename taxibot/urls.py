987780247d9b89463a466f3c58ae80fd3572139c
Khusan Saidvaliev <saidhusan.saidvaliev@gmail.com>
2017-03-08 00:52:35 +0500

    Add DB

--- taxibot/urls.py
+++ taxibot/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [url(r'^$', views.CallListView.as_view(), name='callList'),
                url(r'^callsWithCar/$', views.calls_with_car_ListView.as_view(), name='calls_with_car'),
                url(r'^accepted/$', views.acceptedCalls.as_view(), name='accepted'),
                url(r'^setArrived/$', views.EndCall, name='send_arrived'),
-               url(r'^DBList/$', views.DBCalls, name='db_list'),
+               url(r'^DBList/$', views.DBCalls.as_view(), name='db_list'),
 
                url(r'^webhook/$', views.webhook, name='webhook'),
                url(r'^309803225:AAEfkOtjUfLCTSHicJD05uy7AvilTCkzOYs/$', views.getUpdate, name='get_update'),
