f424bf78fa44c1a1f0d7c25b1ab93c5e67d8823e
Khusan Saidvaliev <saidhusan.saidvaliev@gmail.com>
2017-03-08 01:16:12 +0500

    Add DB

--- taxibot/views.py
+++ taxibot/views.py
@@ -102,7 +102,7 @@ class DBCalls(generic.ListView):    # History of cars
         return call_list
 
 def clearDB(request):  # delete all calls
-    #try:
+    try:
         for call in TaxiCall.objects.all():
             new = TaxiCallHistory.objects.create(call_id=call.call_id, type=call.type, number=call.number,call_time = call.call_time,
                                            details=call.details,address=call.address,IsMap=call.IsMap,longitude=call.longitude,latitude=call.latitude)
@@ -116,11 +116,11 @@ def clearDB(request):  # delete all calls
                new.car_set.create(car_type=car.car_type,car_number=car.car_number,car_time=car.car_time)
                new.save()
 
-   # except:
-     #   print("there are no selected calls")
+    except:
+        print("there are no selected calls")
 
-    #TaxiCall.objects.all().delete()
-        return HttpResponseRedirect(reverse("taxibot:callList"))
+    TaxiCall.objects.all().delete()
+    return HttpResponseRedirect(reverse("taxibot:callList"))
 
 def clearDBHistory(request):  # delete all calls
     TaxiCallHistory.objects.all().delete()
